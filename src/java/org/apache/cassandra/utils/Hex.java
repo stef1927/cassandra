0a8ea3d7c895f0701668a2e1222bc80bdd3923bb
Ariel Weisberg <ariel@weisberg.ws>
2015-05-06 16:50:47 -0400

    For CASSANDRA-9241, in some cases add calculation for arrayOffset() and position(), in others just add asserts that the offset is 0

--- src/java/org/apache/cassandra/utils/Hex.java
+++ src/java/org/apache/cassandra/utils/Hex.java
@@ -19,6 +19,7 @@ package org.apache.cassandra.utils;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import java.nio.ByteBuffer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -70,10 +71,15 @@ public class Hex
 
     public static String bytesToHex(byte... bytes)
     {
-        char[] c = new char[bytes.length * 2];
-        for (int i = 0; i < bytes.length; i++)
+        return bytesToHex(bytes, 0, bytes.length);
+    }
+
+    public static String bytesToHex(byte bytes[], int offset, int length)
+    {
+        char[] c = new char[length * 2];
+        for (int i = 0; i < length; i++)
         {
-            int bint = bytes[i];
+            int bint = bytes[i + offset];
             c[i * 2] = byteToChar[(bint & 0xf0) >> 4];
             c[1 + i * 2] = byteToChar[bint & 0x0f];
         }
@@ -96,7 +102,7 @@ public class Hex
             try
             {
                 s = stringConstructor.newInstance(0, c.length, c);
-            } 
+            }
             catch (InvocationTargetException ite) {
                 // The underlying constructor failed. Unwrapping the exception.
                 Throwable cause = ite.getCause();
