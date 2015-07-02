fbfdcf3059fe3209a07de9eb4182bef611e31b81
Stefania Alborghetti <stefania.alborghetti@datastax.com>
2015-07-02 15:50:55 +0800

    remove StorageProxy.OPTIMIZE_LOCAL_REQUESTS, CASSANDRA-9697

--- src/java/org/apache/cassandra/service/StorageProxy.java
+++ src/java/org/apache/cassandra/service/StorageProxy.java
@@ -69,7 +69,6 @@ public class StorageProxy implements StorageProxyMBean
 {
     public static final String MBEAN_NAME = "org.apache.cassandra.db:type=StorageProxy";
     private static final Logger logger = LoggerFactory.getLogger(StorageProxy.class);
-    static final boolean OPTIMIZE_LOCAL_REQUESTS = true; // set to false to test messagingservice path on single node
 
     public static final String UNREACHABLE = "UNREACHABLE";
 
@@ -705,7 +704,7 @@ public class StorageProxy implements StorageProxyMBean
 
     public static boolean canDoLocalRequest(InetAddress replica)
     {
-        return replica.equals(FBUtilities.getBroadcastAddress()) && OPTIMIZE_LOCAL_REQUESTS;
+        return replica.equals(FBUtilities.getBroadcastAddress());
     }
 
 
