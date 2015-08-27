dca8c6ee29daf4a145ab8e857a288f2db15a03d7
Stefania Alborghetti <stefania.alborghetti@datastax.com>
2015-08-27 10:59:35 +0800

    8741: handle exception

--- src/java/org/apache/cassandra/tools/NodeTool.java
+++ src/java/org/apache/cassandra/tools/NodeTool.java
@@ -1470,6 +1470,9 @@ public class NodeTool
             } catch (InterruptedException e)
             {
                 throw new RuntimeException("Error decommissioning node", e);
+            } catch (UnsupportedOperationException e)
+            {
+                throw new IllegalStateException("Unsupported operation: " + e.getMessage(), e);
             }
         }
     }
