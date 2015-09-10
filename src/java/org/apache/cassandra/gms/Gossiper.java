98b829ccbfa470a30eec0d300f16b9c39473ad59
Stefania Alborghetti <stefania.alborghetti@datastax.com>
2015-09-10 11:01:37 +0800

    10231: Null status entries on nodes that crash during decommission of a different node

--- src/java/org/apache/cassandra/gms/Gossiper.java
+++ src/java/org/apache/cassandra/gms/Gossiper.java
@@ -1510,9 +1510,7 @@ public class Gossiper implements IFailureDetectionEventListener, GossiperMBean
     public void addExpireTimeForEndpoint(InetAddress endpoint, long expireTime)
     {
         if (logger.isDebugEnabled())
-        {
             logger.debug("adding expire time for endpoint : {} ({})", endpoint, expireTime);
-        }
         expireTimeEndpointMap.put(endpoint, expireTime);
     }
 
