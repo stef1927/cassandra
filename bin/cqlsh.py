439332ae64290b87a7abd8474b580f49af1d7959
Stefania Alborghetti <stefania.alborghetti@datastax.com>
2015-09-15 12:11:09 +0800

    10318: Update cqlsh COPY for new internal driver serialization interface

--- bin/cqlsh.py
+++ bin/cqlsh.py
@@ -113,7 +113,7 @@ from cassandra.metadata import (ColumnMetadata, KeyspaceMetadata,
                                 TableMetadata, protect_name, protect_names,
                                 protect_value)
 from cassandra.policies import WhiteListRoundRobinPolicy
-from cassandra.protocol import QueryMessage, ResultMessage
+from cassandra.protocol import ProtocolHandler, QueryMessage, ResultMessage
 from cassandra.query import SimpleStatement, ordered_dict_factory
 
 # cqlsh should run correctly when run out of a Cassandra source tree,
@@ -2388,15 +2388,18 @@ class ImportProcess(multiprocessing.Process):
                     fetch_size=None, paging_state=None, timestamp=insert_timestamp)
 
                 request_id = conn.get_request_id()
-                binary_message = query_message.to_binary(
-                    stream_id=request_id, protocol_version=DEFAULT_PROTOCOL_VERSION, compression=None)
+                conn.send_msg(query_message, request_id=request_id, cb=partial(callback, current_record))
+                #binary_message = ProtocolHandler.encode_message(query_message,
+                #                                                stream_id=request_id,
+                #                                                protocol_version=DEFAULT_PROTOCOL_VERSION,
+                #                                                compressor=None)
 
-                # add the message directly to the connection's queue
                 with conn.lock:
                     conn.in_flight += 1
 
-                conn._callbacks[request_id] = partial(callback, current_record)
-                conn.deque.append(binary_message)
+                # add the message directly to the connection's queue
+                #conn._requests[request_id] = (partial(callback, current_record), ProtocolHandler.decode_message)
+                #conn.push(binary_message)
 
                 # every 50 records, clear the pending writes queue and read
                 # any responses we have
