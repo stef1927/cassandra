2daf5257095b478dbff98e4377f3a1a2c5d7253a
Stefania Alborghetti <stefania.alborghetti@datastax.com>
2015-10-09 08:05:55 +0800

    10415: fix cqlsh bugs

--- pylib/cqlshlib/cql3handling.py
+++ pylib/cqlshlib/cql3handling.py
@@ -165,9 +165,13 @@ JUNK ::= /([ \t\r\f\v]+|(--|[/][/])[^\n\r]*([\n\r]|$)|[/][*].*?[*][/])/ ;
          | <boolean>
          | <blobLiteral>
          | <collectionLiteral>
-         | <functionName> <functionArguments>
+         | <functionLiteral> <functionArguments>
          | "NULL"
          ;
+
+<functionLiteral> ::= (<identifier> ( "." <identifier> )?)
+                 | "TOKEN"
+                 ;
 
 <functionArguments> ::= "(" ( <term> ( "," <term> )* )? ")"
                  ;
@@ -1399,7 +1403,7 @@ def rolename_completer(ctxt, cass):
         return "'%s'" % name
 
     # disable completion for CREATE ROLE.
-    if ctxt.matched[0][0] == 'K_CREATE':
+    if ctxt.matched[0][1].upper() == 'CREATE':
         return [Hint('<rolename>')]
 
     session = cass.session
